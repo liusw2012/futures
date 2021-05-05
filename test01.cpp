1551b1368b28af362d68ec7003a067b3a6011bee
liusw <hsliusw@hotmail.com>
2021-05-05 15:23:08 +0800

    c++11 auto test

---
+++ test01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector< vector<int> > v;
+	vector<int> v1 = {1,2,3,4,5};
+	vector<int> v2 = {6,7,8,9,10};
+	v.push_back(v1);
+	v.push_back(v2);
+	for (auto i = v.begin(); i != v.end(); i++)
+	{
+		for (auto j = i->begin(); j != i->end(); j++)
+		{
+			std::cout << *j << " " << std::endl;
+		}
+	}
+	return 0;
+}
